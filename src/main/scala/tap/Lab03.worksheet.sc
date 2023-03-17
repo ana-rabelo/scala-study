da7b5bff3dc55e5519d5d93c5c8158e7977bee45
Ana Paula Rabelo <prabelo79@gmail.com>
2023-03-17 09:50:55 +0000

    resolução do laboratório 3

--- src/main/scala/tap/Lab03.worksheet.sc
+++ src/main/scala/tap/Lab03.worksheet.sc
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 //  #1
 val aList:List[Int] = List(1, 2, 3)
 val bList=List("edom", "odsoft", "tap")
@@ -14,12 +15,13 @@ bList.flatten
 bList.map(_.length)
 
 //> c)
-
+bList.flatten :: aList :: Nil
 
 //> d)
+dList :: List(e) :: Nil
 
 //> e)
-
+fList :: List(g.toString()) :: Nil 
 
 //  #2
 //> a) List.range
@@ -44,3 +46,23 @@ for (elem <- l if elem.startsWith("Jo")) yield elem
 val x = (i: Int) => if (i < 0) -i else i
 List(1, 2, 3, -1, -2, -3, 0).map(x)
 //> Result: List[Int] = List(1, 2, 3, 1, 2, 3, 0)
+
+def applyF_[A,B](la: List[A],lb:List[B])(f: (List[A],List[B]) => String) : String =
+    f(la,lb)
+
+//res41: String = 19aa2b3c
+def f1[A,B](la: List[A],lb:List[B]):String = 
+    (la zip lb).flatMap(tuple => Seq(tuple._1, tuple._2)).mkString
+
+def f2[A,B](la: List[A], lb:List[B]): String = la.concat(lb).mkString
+
+def f3[A,B](la: List[A], lb:List[B]): String = (la.reverse).concat(lb).mkString
+
+// 3291aabc
+def f4[A,B](la: List[A], lb:List[B]): String = 
+    (la.reverse).take(2).mkString + 
+    ().toString.map(_.asDigit).mkString +
+    lb.mkString
+
+
+applyF_(List(19, 2, 3), List("aa", "b", "c"))(f4) 
--- src/main/scala/tap/lab03.scala
+++ src/main/scala/tap/lab03.scala
@@ -5,14 +5,15 @@ import scala.annotation.tailrec
 object lab03:
   
   // #5
-  def hasDigit[A](s: A):Boolean = ???
+  def hasDigit[A](s: A):Boolean =
+    s.toString().exists(_.isDigit)
 
   // #6
   def max_(xs: List[Int]): Option[Int] =
     @tailrec
     def maxAux(m: Int, ys: List[Int]): Int  = ys match 
-      case Nil => ???
-      case x :: t => ???
+      case Nil => m
+      case x :: t => if (x > m) maxAux(x, t) else maxAux(m, t)
           
     if (xs.isEmpty) None else Some(maxAux(Int.MinValue,xs)) 
 
@@ -20,15 +21,15 @@ object lab03:
   def applyF_[A,B](la: List[A],lb:List[B])(f: (List[A],List[B]) => String) : String =
     f(la,lb)
 
-  def f1[A,B](la: List[A],lb:List[B]):String =
-    ???
+  def f1[A,B](la: List[A],lb:List[B]):String = 
+    (la.map(_.toString) zip lb).flatMap(x  => Seq(x._1, x._2)).mkString
 
-  def f2[A,B](la: List[A],lb:List[B]):String =
-    ???
+  def f2[A,B](la: List[A],lb:List[B]):String = la.concat(lb).mkString
 
-  def f3[A,B](la: List[A],lb:List[B]):String =
-    ???
+  def f3[A,B](la: List[A],lb:List[B]):String = (la.reverse).concat(lb).mkString
 
   def f4[A,B](la: List[A],lb:List[B]):String =
     ???
+
+
 
